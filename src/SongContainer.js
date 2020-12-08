7f9346b6819ac0358797556af7be7fb508b967fb
Mohan_Palat <palatster@gmail.com>
2020-12-08 18:08:08 -0500

    Frontend Commit 05 Delete and Add Routes Completed

--- src/SongContainer.js
+++ src/SongContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SongList from './SongList';
+import CreateSongForm from './CreateSongForm';
 
 class SongContainer extends Component {
   constructor(props) {
@@ -27,9 +28,52 @@ class SongContainer extends Component {
     }
   };
 
-  render() {
-    return <SongList songs={this.state.songs} />;
+  addSong = async (e, song) => {
+    e.preventDefault();
+    console.log(song);
+
+    try {
+      // The createdSongResponse variable will store the response from the Flask API
+      const createdSongResponse = await axios.post(
+        process.env.REACT_APP_FLASK_API_URL + '/api/v1/songs/',
+        song
+      );
+
+      // we are emptying all the songs that are living in state into a new array,
+      // and then adding the song we just created to the end of it
+      // the new song which is called parsedResponse.data
+
+      console.log(createdSongResponse.data.data, ' this is response');
+      this.setState({
+        songs: [...this.state.songs, createdSongResponse.data.data],
+      });
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  deleteSong = async (id) => {
+    console.log(id);
+    const deleteSongResponse = await axios.delete(
+      `${process.env.REACT_APP_FLASK_API_URL}/api/v1/songs/${id}`
+    );
+    console.log(deleteSongResponse);
+    // Now that the db has deleted our item, we need to remove it from state
+    // Then make the delete request, then remove the song from the state array using filter
+    this.setState({ songs: this.state.songs.filter((song) => song.id !== id) });
+
+    console.log(deleteSongResponse, ' response from Flask server');
+  };
+
+  render(){
+    return (
+        <>
+            <SongList songs={this.state.songs} deleteSong={this.deleteSong}/>
+            <CreateSongForm addSong={this.addSong}/>
+        </>
+      )
   }
 }
 
 export default SongContainer;
+
